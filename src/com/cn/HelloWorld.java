f64970a947a08670f04ac24439d49e378008d44a
lihongxuan <l007007007007@126.com>
2015-03-19 18:15:57 +0800

    反反复复

--- src/com/cn/HelloWorld.java
+++ src/com/cn/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.cn;
 public class HelloWorld {
 	public static void main(String [] args){
-		System.out.print("HelloWorld");
+		System.out.print("HelloWorld1111");
 		
 		System.out.print("HelloWorld1");
 		
