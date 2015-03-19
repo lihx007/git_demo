9596e14d6f9f1b8f1f8c72a3031a0d790dde4252
lihongxuan <l007007007007@126.com>
2015-03-19 16:51:39 +0800

    修改的system

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,10 @@ public class HelloWorld {
 	public static void main(String [] args){
 		System.out.print("HelloWorld");
 		
+		System.out.print("HelloWorld1");
+		
+		System.out.print("HelloWorld3");
+		
 	}
 
 }
